75676258d6b80f92b2b04862896a97a1f8289319
Журавлёв Денис <90453519+DenZhICT@users.noreply.github.com>
2024-10-23 21:54:57 +0300

    add sleep

--- project/test/test_server.py
+++ project/test/test_server.py
@@ -1,12 +1,15 @@
 import socket
 import subprocess
 import os
+import time
 
 def test_env_password():
     assert os.environ['INPUT_PASSWORD'] == 'admin'
 
 def test_connection():
     subprocess.Popen(["python", "./project/src/server.py"])
+
+    time.sleep(30)
 
     client = socket.socket()
     client.connect(('localhost', 6565))
