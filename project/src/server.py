aaf1a6c8341fe35c6062328195f9dd65504ff088
Журавлёв Денис <deniszhict@gmail.com>
2024-10-23 20:17:50 +0300

    1 connection server change

--- project/src/server.py
+++ project/src/server.py
@@ -4,13 +4,10 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server.bind(('localhost', 6565))
 server.listen(1)
-while True:
-    con, adr = server.accept()
-    data = con.recv(1024)
-    if not data:
-        con.close()
-        break
-    con.send(data)
-    con.close()
+
+con, adr = server.accept()
+data = con.recv(1024)
+con.send(data)
+con.close()
 
 server.close()
