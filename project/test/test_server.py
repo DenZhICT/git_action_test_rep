8d0819d13bc7692c4ab9316c60a9843ba6b7739f
Журавлёв Денис <90453519+DenZhICT@users.noreply.github.com>
2024-10-23 21:50:17 +0300

    Update test_server.py

--- project/test/test_server.py
+++ project/test/test_server.py
@@ -6,7 +6,7 @@ def test_env_password():
     assert os.environ['INPUT_PASSWORD'] == 'admin'
 
 def test_connection():
-    subprocess.Popen(["python", "./project/src/server.py"])
+    subprocess.run(["python", "./project/src/server.py"])
 
     client = socket.socket()
     client.connect(('localhost', 6565))
