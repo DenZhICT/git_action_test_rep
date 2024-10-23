63e4da7a409620171020191a6c2eb444a04e1f30
Журавлёв Денис <deniszhict@gmail.com>
2024-10-23 18:09:37 +0300

    create server and test

---
+++ project/test/test_server.py
@@ -0,0 +1,15 @@
+import socket
+import os
+
+def test_env_password():
+    assert os.environ['INPUT_PASSWORD'] == 'admin'
+
+def test_connection():    
+    client = socket.socket()
+    client.connect(('localhost', 6565))
+
+    message = "Massage123!"
+    client.send(message.encode())
+    data = client.recv(1024)
+    
+    assert data.decode() == message
